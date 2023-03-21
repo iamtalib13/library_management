4d59970d72f2f288111e1be02754708c579e0432
iamtalib13 <talibsheikh16@gmail.com>
2023-03-21 11:21:24 +0530

    Added Library Management

---
+++ library_management/library_management/doctype/library_management/library_management.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Talib and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Library Management", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ library_management/library_management/doctype/library_management/test_library_management.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Talib and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLibraryManagement(FrappeTestCase):
+	pass
