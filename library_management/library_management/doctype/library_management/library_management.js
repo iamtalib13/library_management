36e1c39d5d4a593deed4aa5af476f9e5fec7cc33
iamtalib13 <talibsheikh16@gmail.com>
2023-03-21 11:30:42 +0530

    Added

--- library_management/library_management/doctype/library_management/library_management.js
+++ library_management/library_management/doctype/library_management/library_management.js
@@ -1,8 +1,12 @@
-// Copyright (c) 2023, Talib and contributors
-// For license information, please see license.txt
 
-// frappe.ui.form.on("Library Management", {
-// 	refresh(frm) {
 
-// 	},
-// });
+frappe.ui.form.on("Library Management", {
+	refresh(frm) {
+
+	},
+
+
+    first_name(frm){
+        frappe.msgprint("First name")
+    }
+});
